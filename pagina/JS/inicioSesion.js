c957f9700668586ef00fbfc2b681544b77263210
luciarinconpozo <luciarincon58@gmail.com>
2024-05-14 09:55:32 +0200

    act

--- pagina/JS/inicioSesion.js
+++ pagina/JS/inicioSesion.js
@@ -1,9 +1,6 @@
-window.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("load", function () {
     const divs = this.document.querySelectorAll(".form_div");
-    const inputcaptcha = document.querySelector(".captcha_form input");
-    
-    const login = document.querySelector("form .form_button");
-    
+        
     divs.forEach(div => {
         div.addEventListener("click", function() {
             div.querySelector(".form_label").classList.add("activelabel");
