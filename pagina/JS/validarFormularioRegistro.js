edc521a1f7f040b157c9bfda2d7dc289d7c4d5d3
luciarinconpozo <luciarincon58@gmail.com>
2024-05-17 18:44:10 +0200

    acabado

--- pagina/JS/validarFormularioRegistro.js
+++ pagina/JS/validarFormularioRegistro.js
@@ -1,18 +1,44 @@
-window.addEventListener("load", function () {
+window.addEventListener("load", function () {
     const divs = this.document.querySelectorAll(".form_div");
-    const inputbox = document.querySelectorAll(".form_input");
     const inputcaptcha = document.querySelector(".captcha_form input");
-    
+
     const login = document.querySelector("form .form_button");
-    
+
     divs.forEach(div => {
-        div.addEventListener("click", function() {
-            div.querySelector(".form_label").classList.add("activelabel");
+        const input = div.querySelector(".form_input");
+        const label = div.querySelector(".form_label");
+
+        input.addEventListener("input", function () {
+            if (input.value !== "") {
+                label.classList.add("activelabel");
+            }
+            else {
+                label.classList.remove("activelabel");
+            }
+        });
+
+        input.addEventListener("focus", function () {
+            label.classList.add("activelabel");
+        });
+
+        // blur si el elemento pierde el foco
+        input.addEventListener("blur", function () {
+            if (input.value === "") {
+                label.classList.remove("activelabel");
+            }
+        });
+
+
+        document.getElementById("botonReset").addEventListener("click", function () {
+            let divErrores = document.getElementById("errores");
+            divErrores.style.innerHTML = "";
+            divErrores.style.display = "none";
+
         });
     }
     );
-    
-    inputcaptcha.addEventListener("keyup", function(){
+
+    inputcaptcha.addEventListener("keyup", function () {
         let userdata = inputcaptcha.value;
         if (userdata.trim() != 0) {
             login.classList.add("active");
@@ -21,9 +47,9 @@ window.addEventListener("load", function () {
             login.classList.remove("active");
         }
     });
-    
-    
-    
+
+
+
     (function () {
         const fonts = ['cursive'];
         let captchavalue = "";
@@ -32,38 +58,38 @@ window.addEventListener("load", function () {
             value = value.substr(0, 5 + Math.random() * 5);
             captchavalue = value;
         }
-    
+
         function setcaptcha() {
             let html = captchavalue.split("").map((char) => {
                 const rotate = -20 + Math.trunc(Math.random() * 50);
                 const font = Math.trunc(Math.random() * fonts.length);
-    
+
                 return `<span style="
                 transform:rotate(${rotate}deg);
                 font-family:${font[font]};
                 ">${char} </span>`;
-                
+
             }).join("");
             document.querySelector(".signin_form #captcha .preview").innerHTML = html;
         }
-    
-    
+
+
         function initcaptcha() {
             document.querySelector(".signin_form #captcha .captcha_refresh").addEventListener("click", function () {
                 gencaptch();
                 setcaptcha();
             });
-    
+
             gencaptch();
             setcaptcha();
         }
-    
+
         initcaptcha();
-    
+
         document.querySelector(".signin_form .form_button").addEventListener("click", function () {
-    
+
             let inputcaptchavalue = document.querySelector(".signin_form #captcha input").value;
-    
+
             if (inputcaptchavalue === captchavalue) {
                 alert("¡CORRECTO!");
             }
@@ -72,9 +98,9 @@ window.addEventListener("load", function () {
             }
         });
     })();
-    
-    
-    }
+
+
+}
 
 );
 
@@ -145,7 +171,7 @@ function mostrarErrores(errores) {
 
     divErrores.appendChild(ul);
 }
-    
+
 
 
 
